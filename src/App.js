8f05564958019f3873cee1ad28b4501ddebf6cb7
doanandreas <intern.doan@sayurbox.com>
2022-06-22 15:53:51 +0700

    hadhe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
 
-function App() {
+const connectToRoom = (id) => {
+  const doc = new Y.Doc();
+  const map = doc.getMap();
+
+  const roomId = "tldraw-p2p-eval";
+  const provider = new WebrtcProvider(roomId, doc);
+
+  map.observe((ymapEvent) => {
+    ymapEvent.target === map; // => true
+
+    // Find out what changed:
+    // Option 1: A set of keys that changed
+    ymapEvent.keysChanged; // => Set<strings>
+    // Option 2: Compute the differences
+    ymapEvent.changes.keys; // => Map<string, { action: 'add'|'update'|'delete', oldValue: any}>
+
+    // sample code.
+    ymapEvent.changes.keys.forEach((change, key) => {
+      if (change.action === "add") {
+        console.log(
+          `Property "${key}" was added. Initial value: "${map.get(key)}".`
+        );
+      } else if (change.action === "update") {
+        console.log(
+          `Property "${key}" was updated. New value: "${map.get(
+            key
+          )}". Previous value: "${change.oldValue}".`
+        );
+      } else if (change.action === "delete") {
+        console.log(
+          `Property "${key}" was deleted. New value: undefined. Previous value: "${change.oldValue}".`
+        );
+      }
+    });
+  });
+
+  doc.transact(() => {
+    map.set(id, new Date().getTime());
+  }, id);
+};
+
+window.connectToRoom = connectToRoom;
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>If you see this page, the Yjs WebRTC code is working correctly.</p>
+        <p>ENV: {process.env.NODE_ENV}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/const.js
@@ -0,0 +1,2 @@
+export const PEER_ID = process.env.NODE_ENV;
+window.PEER_ID = PEER_ID;
